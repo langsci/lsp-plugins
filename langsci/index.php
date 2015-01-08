082e06665de1c47026d0f588359b1dc5f1067fe6
unknown <slilienthal@CEDIS216.cedis.fu-berlin.de>
2015-01-08 17:38:41 +0100

    first commit

---
+++ langsci/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_langsci
+ */
+
+/**
+ * @file plugins/generic/langsci/index.php
+ *
+ * Copyright (c) CeDiS, Freie Universität Berlin
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_cemog
+ * @brief Wrapper for langsci plugin.
+ *
+ */
+
+require_once('CiteAsPlugin.inc.php');
+
+return new CiteAsPlugin();
+
+?>
